b136ddaf58954dcac854cb49c5306c3df676d8f4
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-26 12:39:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-zinc-800 text-amber-50 h-full w-full'>
+      <h1>Notes App </h1>
+    </div>
+  )
+}
+
+export default App
